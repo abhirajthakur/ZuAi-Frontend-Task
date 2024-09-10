f6dc11b825d9652aa8e3f950f3c29f95caba0246
Abhiraj Thakur <abhirajthakur124@gmail.com>
2024-09-11 00:39:01 +0530

    Remove error and redundant files

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -2,28 +2,25 @@
 
 import { Button } from "@/components/ui/button";
 import {
-  Form,
-  FormControl,
-  FormField,
-  FormItem,
-  FormLabel,
+    Form,
+    FormControl,
+    FormField,
+    FormItem,
+    FormLabel,
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
+    Select,
+    SelectContent,
+    SelectItem,
+    SelectTrigger,
+    SelectValue,
 } from "@/components/ui/select";
-import usePdfStore from "@/store/useFileStore";
 import { zodResolver } from "@hookform/resolvers/zod";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
-import { useEffect, useRef, useState } from "react";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
-import { useDropzone } from "react-dropzone";
 
 const FormSchema = z.object({
   courseworkType: z.string(),
